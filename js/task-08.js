7187dc9ea9e3a118c7f0c3476393eafaaf048b3d
James Lynch <jimlynch268@gmail.com>
2023-02-06 13:54:42 -0500

    corrections to task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,25 @@
 const form = document.querySelector(".login-form");
 const checkEmail = document.getElementsByName('email')
 const checkPassword = document.getElementsByName('password')
+const myFormDatarm = document.querySelector('form');
 
 const userArrray = []
-form.addEventListener("submit", (event) => {
+
+form.addEventListener("submit", checkInput) 
+
+function checkInput(event) {
+  checkEmail[0].value = checkEmail[0].value.trim()
+  checkPassword[0].value = checkPassword[0].value.trim()
   event.preventDefault();
-  if (checkEmail[0].value.trim().length === 0 || checkPassword[0].value.trim().length === 0) {
+  if (checkEmail[0].value.length === 0 || checkPassword[0].value.length === 0) {
     alert ("Please fill in all the fields!");
   } else {
 
-    const myFormDatarm = document.querySelector('form');
     const data = new FormData(myFormDatarm);
     const compleatInfo = Object.fromEntries(data)
 
     console.log(compleatInfo)     
   event.currentTarget.reset();
   }
-})
+}
+  
